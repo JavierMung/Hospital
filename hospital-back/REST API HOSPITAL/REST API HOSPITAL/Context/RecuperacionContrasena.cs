2ce093823946dd451b766ef5007fe302cf8e2392
JavierMung <fjmg@123lease.com.mx>
2023-11-25 18:45:06 -0600

    se intenta de nuevo el evitar subir archivos erroneos

---
+++ hospital-back/REST API HOSPITAL/REST API HOSPITAL/Context/RecuperacionContrasena.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REST_API_HOSPITAL.Context;
+
+public partial class RecuperacionContrasena
+{
+    public int IdRecuperacionContrasena { get; set; }
+
+    public int IdUsuario { get; set; }
+
+    public string TokenRecuperacion { get; set; } = null!;
+
+    public DateTime FechaExpiracion { get; set; }
+
+    public bool Utilizado { get; set; }
+
+    public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+}
