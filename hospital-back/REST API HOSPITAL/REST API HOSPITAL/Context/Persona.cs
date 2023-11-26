ed8040fa712888fd8407bfea4be9bb4ee37bb00e
JavierMung <fjmg@123lease.com.mx>
2023-11-25 19:34:20 -0600

    se agrega .gitignore

---
+++ hospital-back/REST API HOSPITAL/REST API HOSPITAL/Context/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REST_API_HOSPITAL.Context;
+
+public partial class Persona
+{
+    public int IdPersona { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string ApellidoPaterno { get; set; } = null!;
+
+    public string? ApellidoMaterno { get; set; }
+
+    public DateTime FechaNacimiento { get; set; }
+
+    public string Calle { get; set; } = null!;
+
+    public string Colonia { get; set; } = null!;
+
+    public string Municipio { get; set; } = null!;
+
+    public string Estado { get; set; } = null!;
+
+    public int Cp { get; set; }
+
+    public int? Telefono { get; set; }
+
+    public virtual ICollection<Trabajador> Trabajadors { get; } = new List<Trabajador>();
+}
---
+++ hospital-back/REST API HOSPITAL/REST API HOSPITAL/Context/RecetaMedica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REST_API_HOSPITAL.Context;
+
+public partial class RecetaMedica
+{
+    public int IdRecetaMedica { get; set; }
+
+    public int IdCita { get; set; }
+
+    public string? Posologia { get; set; }
+
+    public virtual ICollection<Bitacora> Bitacoras { get; } = new List<Bitacora>();
+
+    public virtual Cita IdCitaNavigation { get; set; } = null!;
+
+    public virtual ICollection<RecetaMedicamento> RecetaMedicamentos { get; } = new List<RecetaMedicamento>();
+}
