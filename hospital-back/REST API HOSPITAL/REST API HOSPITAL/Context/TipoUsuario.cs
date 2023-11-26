ad241e4218a9ada45a2670ffb7fb7e6c8f9ec199
JavierMung <fjmg@123lease.com.mx>
2023-11-25 19:15:03 -0600

    se agrega .gitignore

---
+++ hospital-back/REST API HOSPITAL/REST API HOSPITAL/Context/TipoUsuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REST_API_HOSPITAL.Context;
+
+public partial class TipoUsuario
+{
+    public int IdTipoUsuario { get; set; }
+
+    public string TipoUsuario1 { get; set; } = null!;
+}
---
+++ hospital-back/REST API HOSPITAL/REST API HOSPITAL/Controllers/MedicosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using REST_API_HOSPITAL.Interfaces;
+using REST_API_HOSPITAL.ViewModels;
+
+namespace REST_API_HOSPITAL.Controllers
+{
+
+	[Route("[controller]")]
+	[ApiController]
+	public class MedicosController : ControllerBase
+	{
+		private readonly IMedicosServices _medicosServices;
+
+		public MedicosController(IMedicosServices medicosServices)
+		{
+			_medicosServices = medicosServices;
+		}
+
+		[HttpGet("obtenerMedico/{id}")]
+		public async Task<ActionResult<ViewMedicos>> GetMedico(int id)
+		{
+			if (id == 0)
+			{
+				return StatusCode(StatusCodes.Status400BadRequest, "El ID es incorrecto");
+			}
+			try
+			{
+				var res = await _medicosServices.GetMedico(id);
+				if (res == null)
+				{
+					return StatusCode(StatusCodes.Status204NoContent, "No existen medicos con ese ID. intentelo con otro ID por favor");
+				}
+				return res;
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status501NotImplemented, "Error en el servidor: "+ex.Message+"Intentelo mas tarde");
+			}
+		}
+	}
+}
